388b21f2f54278da23ffec78057921f43b221365
PANKAJA D.L.K <pankajadlk.23@uom.lk>
2025-04-11 21:41:29 +0530

    update guest file

--- src/model/Guest.java
+++ src/model/Guest.java
@@ -1,5 +1,36 @@
 package model;
 
 public class Guest {
-    
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Guest(String id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest: " + name + " (ID: " + id + ")";
+    }
 }
